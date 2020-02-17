150a6e4330e7ed4e3687bf31862abcd73a36c719
Darko Filipovski <darkofilipovski0@gmail.com>
2020-02-17 10:00:20 +0100

    Added checkpoint saving and loading

--- src/models/DCGAN_trainer.py
+++ src/models/DCGAN_trainer.py
@@ -8,8 +8,9 @@ from datasets.celeba_dataset import get_image_dataset
 
 
 class DCGANTrainer:
-    def __init__(self, data_root, num_epochs=100):
+    def __init__(self, data_root, checkpoints_path, num_epochs=100):
         self.config = _C
+        self.checkpoints_path = checkpoints_path
         self.num_epochs = num_epochs
         self.device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
@@ -20,31 +21,36 @@ class DCGANTrainer:
                                                       batch_size=_C.miniBatchSize,
                                                       shuffle=True, num_workers=2)
 
-    def train(self):
-        # Initialize BCELoss function
-        criterion = nn.BCELoss()
+        # Setup Adam optimizers for both G and D
+        self.optimizerD = optim.Adam(self.netD.parameters(), lr=self.config.baseLearningRate,
+                                     betas=(self.config.beta1, 0.999))
+        self.optimizerG = optim.Adam(self.netG.parameters(), lr=self.config.baseLearningRate,
+                                     betas=(self.config.beta1, 0.999))
+
+        # Lists to keep track of progress
+        self.img_list = []
+        self.G_losses = []
+        self.D_losses = []
+        self.start_epoch = 0
 
         # Create batch of latent vectors that we will use to visualize
         #  the progression of the generator
-        fixed_noise = torch.randn(64, self.config.dimLatentVector, 1, 1, device=self.device)
+        self.fixed_noise = torch.randn(64, self.config.dimLatentVector, 1, 1, device=self.device)
+
+    def train(self):
+        # Initialize BCELoss function
+        criterion = nn.BCELoss()
 
         # Establish convention for real and fake labels during training
         real_label = 1
         fake_label = 0
 
-        # Setup Adam optimizers for both G and D
-        optimizerD = optim.Adam(self.netD.parameters(), lr=self.config.baseLearningRate, betas=(self.config.beta1, 0.999))
-        optimizerG = optim.Adam(self.netG.parameters(), lr=self.config.baseLearningRate, betas=(self.config.beta1, 0.999))
-
         # Lists to keep track of progress
-        img_list = []
-        G_losses = []
-        D_losses = []
         iters = 0
 
         print("Starting Training Loop...")
         # For each epoch
-        for epoch in range(self.num_epochs):
+        for epoch in range(self.start_epoch, self.num_epochs):
             # For each batch in the dataloader
             for i, data in enumerate(self.dataloader, 0):
 
@@ -81,7 +87,7 @@ class DCGANTrainer:
                 # Add the gradients from the all-real and all-fake batches
                 errD = errD_real + errD_fake
                 # Update D
-                optimizerD.step()
+                self.optimizerD.step()
 
                 ############################
                 # (2) Update G network: maximize log(D(G(z)))
@@ -96,7 +102,7 @@ class DCGANTrainer:
                 errG.backward()
                 D_G_z2 = output.mean().item()
                 # Update G
-                optimizerG.step()
+                self.optimizerG.step()
 
                 # Output training stats
                 if i % 50 == 0:
@@ -105,13 +111,51 @@ class DCGANTrainer:
                              errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
                 # Save Losses for plotting later
-                G_losses.append(errG.item())
-                D_losses.append(errD.item())
+                self.G_losses.append(errG.item())
+                self.D_losses.append(errD.item())
 
                 # Check how the generator is doing by saving G's output on fixed_noise
                 if (iters % 500 == 0) or ((epoch == self.num_epochs - 1) and (i == len(self.dataloader) - 1)):
                     with torch.no_grad():
-                        fake = self.netG(fixed_noise).detach().cpu()
-                    img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                        fake = self.netG(self.fixed_noise).detach().cpu()
+                    self.img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
                 iters += 1
+
+    def save_checkpoint(self, epoch):
+        checkpoint = {
+            'G': {
+                'model_state_dict': self.netG.state_dict(),
+                'optim_state_dict': self.optimizerG.state_dict()
+            },
+            'D': {
+                'model_state_dict': self.netD.state_dict(),
+                'optim_state_dict': self.optimizerD.state_dict()
+            },
+            'epoch': epoch,
+            'G_losses': self.G_losses,
+            'D_losses': self.D_losses,
+            'img_list': self.img_list,
+            'fixed_noise': self.fixed_noise
+        }
+
+        torch.save(checkpoint, self.checkpoints_path)
+
+        print(f'[+] Checkpoint saved! Epoch {epoch}.')
+
+    def load_checkpoint(self, checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+
+        self.netG.load_state_dict(checkpoint['G']['model_state_dict'])
+        self.optimizerG.load_state_dict(checkpoint['G']['optim_state_dict'])
+
+        self.netD.load_state_dict(checkpoint['D']['model_state_dict'])
+        self.optimizerD.load_state_dict(checkpoint['D']['optim_state_dict'])
+
+        self.start_epoch = checkpoint['epoch']
+        self.G_losses = checkpoint['G_losses']
+        self.D_losses = checkpoint['D_losses']
+        self.img_list = checkpoint['img_list']
+        self.fixed_noise = checkpoint['fixed_noise']
+
+        print(f'[+] Checkpoint loaded! Epoch {self.start_epoch}.')
